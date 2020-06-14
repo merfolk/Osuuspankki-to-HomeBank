17c1adb4e21b6b2715a645a419f3de6561c6ba9d
Matti Bragge <1806447+merfolk@users.noreply.github.com>
2020-06-14 10:59:47 +0300

    Split classes into their own files

---
+++ PaymentType.cs
@@ -0,0 +1,18 @@
+﻿namespace osuuspankki_import
+{
+    internal enum PaymentType {
+        None = 0,
+        CreditCard = 1,
+        //Cheque = 2,
+        Cash = 3,
+        Transfer = 4,
+        //Internal Transfer = 5 // does not work
+        DebitCard = 6,
+        // Standing Order = 7,
+        //Electronic Payment = 8,
+        Deposit = 9,
+        //FI_fee = 10,
+        //DirectDebit = 11
+
+    }
+}
