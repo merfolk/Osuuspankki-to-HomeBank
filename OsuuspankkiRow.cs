7be67acc0d270b45a19d78b9c5198830cda20741
Matti Bragge <1806447+merfolk@users.noreply.github.com>
2021-01-02 11:04:05 +0200

    Disable warnings about "unused" properties

--- OsuuspankkiRow.cs
+++ OsuuspankkiRow.cs
@@ -3,6 +3,7 @@
 namespace osuuspankki_import
 {
     internal class OsuuspankkiRow {
+        # pragma warning disable CS0649
         internal DateTime Date;
         internal Decimal Amount;
         internal int PaymentType;
@@ -12,5 +13,6 @@ namespace osuuspankki_import
         internal String Reference;
         internal String Message;
         internal String ArchiveId;
+        # pragma warning restore CS0649
     }
 }
